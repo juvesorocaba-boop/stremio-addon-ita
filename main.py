a5e8480efa64b13df2f3c8e82869c6933357880d
juvesorocaba-boop <juvesorocaba@gmail.com>
2026-07-10 11:21:06 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,27 +3,10 @@ MANIFEST = {
     "version": "1.0.0",
     "name": "Cinema Italiano",
     "description": "La collezione definitiva di film, serie e canali TV italiani in streaming.",
-    "logo": "https://github.com/juvesorocaba-boop/stremio-addon-ita/blob/main/logo.png",
+    "logo": "https://raw.githubusercontent.com/juvesorocaba-boop/stremio-addon-ita/main/logo.png",
     "types": ["movie", "series", "channel"],
     "catalogs": [
         {
-            "type": "movie",
-            "id": "film_italiani",
-            "name": "Film Italiani"
-        },
-        {
-            "type": "series",
-            "id": "serie_italiane",
-            "name": "Serie Italiane"
-        },
-        {
-            "type": "channel",
-            "id": "canali_tv",
-            "name": "Canali TV"
-        }
-    ],
-    "resources": ["catalog", "meta", "stream"]
-}
             "type": "movie",
             "id": "film_italiani",
             "name": "Film Italiani"
